942204385905921d43523f26337e58d0410a2bef
Francesco <francesco.dente2003@gmail.com>
2024-07-04 14:59:08 +0200

    1.0.1: bonus: ok, jumbo uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import Jumbo from './components/Jumbo.vue';
 
 export default {
   components: {
     AppTitle,
+    Jumbo,
   }
 }
 </script>
 
 <template>
+  <Jumbo />
   <AppTitle />
 </template>
 
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,22 @@
+<script>
+   export default {
+      data() {
+         return {
+            imageName: "jumbo.jpg"
+         };
+      },
+      methods: {
+         getImgPath: function(path) {
+            return new URL(path, import.meta.url).href;
+         }
+      }
+   };
+</script>
+
+<template>
+   <img :src="getImgPath(`../assets/${imageName}`)">
+</template>
+
+<style scoped>
+
+</style>
